c3d472553f41d9badbd05f3d24b6fbd1adca1c63
vsvld <v.alyokhin@gmail.com>
2014-12-27 15:42:34 +0200

    lalala

--- src/com/javarush/test/level19/lesson05/task02/Solution.java
+++ src/com/javarush/test/level19/lesson05/task02/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.test.level19.lesson05.task02;
 Считать с консоли имя файла.
 Вывести в консоль количество слов "world", которые встречаются в файле.
 Закрыть поток ввода.
+
+c:/tmp/world.txt
 */
 
 import java.io.*;
@@ -12,13 +14,14 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) {
+
         try (BufferedReader readerConsole = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader readerFile = new BufferedReader(new FileReader (readerConsole.readLine()))) {
 
             int count = 0;
 
             while (readerFile.ready()) {
-                Pattern pattern = Pattern.compile("[wW]orld");
+                Pattern pattern = Pattern.compile("\\b[wW]orld\\b");
                 Matcher matcher = pattern.matcher(readerFile.readLine());
                 while (matcher.find()) count++;
             }
