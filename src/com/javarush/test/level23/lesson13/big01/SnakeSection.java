d79952695a7dde623c9f01235f5ff7c79d7bea5e
vsvld <v.alyokhin@gmail.com>
2015-01-29 19:42:07 +0200

    level 23 big task (Snake)

---
+++ src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
@@ -0,0 +1,44 @@
+package com.javarush.test.level23.lesson13.big01;
+
+public class SnakeSection
+{
+    private int x;
+    private int y;
+
+
+    public SnakeSection(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SnakeSection section = (SnakeSection) o;
+
+        if (x != section.x) return false;
+        if (y != section.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+}
